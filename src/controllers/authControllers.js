edb0ce91e34b1936b2a8cf1454c950d0aa3e6efc
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-10 02:24:49 -0300

    fix

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -4,13 +4,14 @@ export const registerController = (req, res) => {
         const JWT_SECRET = process.env.JWT_SECRET
 
         const { userName, email, password } = req.body
-        console.log(userName, email, password)
+        console.log("Datos recibidos en /register:", { userName, email, password })
 
-        
-
-       
+        res.status(200).json({
+            message: "Datos recibidos correctamente",
+            data: { userName, email }
+        })
     } catch (error) {
-        console.log(error)
+        console.log("Error en registerController:", error)
         res.status(500).json({ message: 'Error al registrar usuario' })
     }
 }
