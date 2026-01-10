8c44a47de4cbb85a507217f1ca7ea245287f203b
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-09 23:22:41 -0300

    test

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,32 +2,32 @@
 export const registerController = (req, res) => {
     try {
         const JWT_SECRET = process.env.JWT_SECRET
-        const { email, password } = req.body
+        // const { email, password } = req.body
 
-        if (!email || !password) {
-            return res.status(400).json({ message: 'Faltan datos' })
-        }
+        // if (!email || !password) {
+        //     return res.status(400).json({ message: 'Faltan datos' })
+        // }
 
-        const user = await User.findOne({ email })
+        // const user = await User.findOne({ email })
 
-        if (user) {
-            return res.status(400).json({ message: 'Usuario ya existe' })
-        }
+        // if (user) {
+        //     return res.status(400).json({ message: 'Usuario ya existe' })
+        // }
 
-        const hashPassword = await bcrypt.hash(password, 10)
+        // const hashPassword = await bcrypt.hash(password, 10)
 
-        const newUser = new User({
-            email,
-            password: hashPassword
-        })
+        // const newUser = new User({
+        //     email,
+        //     password: hashPassword
+        // })
 
-        await newUser.save()
+        // await newUser.save()
 
-        const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' })
+        // const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' })
 
-        res.status(200).json({ token })
+        // res.status(200).json({ token })
 
-        console.log(token)
+        console.log(JWT_SECRET)
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Error al registrar usuario' })
