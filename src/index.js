40d92fc606ccb0c155c5cb4973059a2719a238d8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-10 03:03:19 -0300

    add cookieParser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { connectDB, disconnectDB } from './config/confingdb.js'
 
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 // Routes
 import authRoutes from './routes/authRoutes.js'
