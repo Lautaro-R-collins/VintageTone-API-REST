cced90648e0027611c52053c6f10ebe56ed121f0
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-16 00:45:42 -0300

    add review in index

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 // Routes
 import authRoutes from './routes/authRoutes.js'
 import productsRoutes from './routes/productsRoutes.js'
+import reviewRoutes from './routes/reviewRoutes.js'
 
 
 app.get('/', (req, res) => {
@@ -44,6 +45,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productsRoutes)
+app.use('/api/reviews', reviewRoutes)
 
 
 // Middleware de manejo de errores global
